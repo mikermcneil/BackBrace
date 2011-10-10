fa035b8df07e4ea5e74ff78e1a1c0ef1e1b68012
mikermcneil <michael.r.mcneil@gmail.com>
2011-10-10 14:18:22 -0500

    Added cleaner error and submit events.  Added some default behavior to the example to show an example of overriding.

--- backbrace.js
+++ backbrace.js
@@ -8,37 +8,6 @@
  * jquery
  */
 
-
-// Adds a max and minimum to backbone collections
-Backbone.Collection = Backbone.Collection.extend({
-	max: 100,
-	min: 0,
-	// Add without overflowing
-	safeAdd: function (o) {
-		if (this.length < this.max) {
-			this.add(o);
-			if (this.length==this.max && this.onFull) this.onFull();
-			return true;
-		} else return false;
-	},
-	// Remove without overflowing
-	safeRemove: function (o) {
-		if (o && o.length && this.length-o.length >= this.min) {
-			this.remove(o);
-			return true;
-		}
-		else if (o && this.length-1 >= this.min) {
-			this.remove(o)
-			return true;
-		}
-		else
-			return false;
-	}
-})
-
-
-
-//
 //
 // When you extend this class:
 //
@@ -46,14 +15,142 @@ Backbone.Collection = Backbone.Collection.extend({
 //
 // 2) set this.model = the backbone class name of your model (i.e. Message)
 //
-// 3) There is a map "rules" in the backbone model
+// 3) There is a map "rules" in the backbone model.  List them in order of display preference
 //
 // 4) All form fields must have classes 'field' and $fieldName where $fieldname == the corresponding
 //		attribute in the backbone model.  If you assign a validation rule that and $fieldName
 //		which is not in the model, you can still use custom validation (things like reentering your password)
 //
-// 5) Overwrite decorateField and undecorateField to control what happens when an error is detected (and resolved)
+// 5) Override decorateField and undecorateField to control what happens when an error is detected (and resolved)
 //
+// 6) Override handleForm to control your form's submit behavior
+//
+/**
+ * Form view
+ */
+Backbone.Form = Backbone.View.extend({
+	dataKey: 'backbraceinput',
+
+	
+	/**
+	 * Presentation logic for error reporting
+	 * element -> the form element that is naughty
+	 * error   -> the string key of the validation fail that was triggered
+	 *				( i.e. required, email, url, minLength, etc. )
+	 *
+	 *	Some sensible defaults are included here that depend on the
+	 *	form convention outlined in example.html.
+	 */
+	decorateField: function(element,error) {
+		element.addClass('error');
+		element.parent().addClass('error');
+		element.parent().children('span.message').addClass('error').show().text(error[0].message);
+	},
+	undecorateField: function(element,error) {
+		element.removeClass('error');
+		element.parent().removeClass('error');
+		element.parent().children('span.message').addClass('error').hide().text('');
+	},
+
+	/**
+	 * Submit-- fired when form is successfully submitted
+	 */
+	submit: function() {
+
+		// Override this with a method that handles the form here.
+		alert("Backbrace default form handler triggered.");
+
+		// To allow traditional form submission, return true
+		// Default is to override
+		return false;
+	},
+	/**
+	 * Error-- fired when form submission is attempted, but fails
+	 */
+	error: function () {
+	},
+	//////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+	events: {
+		"focus .field": "focusField",
+		"blur .field": "blurField",
+		"change .field":"changeField",
+		"submit":"trySubmit"
+	},
+	fields: {},
+	initialize: function () {
+		this.model = new (this.model)()
+		this.el = $(this.el);
+
+		// Populate fields
+		var view = this;
+		_.each(this.model.rules,function (value,field) {
+			var element = view.el.find(".field."+field);
+			element.data(view.dataKey,field);
+			view.fields[field]=element;
+		})
+		_.bindAll(this);
+	},
+	focusField: function (e) {
+		this.undecorateField($(e.currentTarget));
+	},
+	blurField: function (e) {
+		this.validateField($(e.currentTarget));
+	},
+	changeField: function (e) {
+		this.validateField($(e.currentTarget));
+	},
+	trySubmit: function(e) {
+		if (!this.validateFields()) {
+			if (this.submit())
+				return true;
+		}
+		else
+			this.error();
+		
+		// Prevent form submission
+		e.preventDefault();
+		return false;
+	},
+	validateField: function(element) {
+		var field = element.data(this.dataKey),
+		input = this.fields[field],
+		hasError;
+		if (field) {
+			hasError = this.model.validateOne(field,input.val());
+			this.renderField(hasError,input,field);
+		}
+		return !field || hasError;
+	},
+	renderField: function(error,elem,field) {
+		if (error)
+			this.decorateField(elem,error);
+		else
+			this.undecorateField(elem,error);
+	},
+	validateFields: function () {
+		// Get values from form
+		var view = this;
+		var data= {},
+		hasError = false;
+		_.each(this.model.rules,function (rule,field) {
+			var elem = view.el.find(".field."+field);
+			hasError = view.validateField(elem) || hasError;
+			data[field] = view.el.find(".field."+field).val();
+		});
+		this.model.set(data);
+		return hasError;
+	}
+});
+
+
+
+
+// Extensions to the model that support validation
 Backbone.Model = Backbone.Model.extend({
 	
 	validateOne: function (field,value) {
@@ -96,6 +193,33 @@ Backbone.Model = Backbone.Model.extend({
 
 	// adapted from https://github.com/n-time/backbone.validation/blob/master/backbone.validations.js
 	validators: {
+		// Error map
+		errors: {
+			required: {
+				message: 'That field is required.'
+			},
+			email: {
+				message: 'Invalid email.'
+			},
+			url: {
+				message: 'Invalid URL.'
+			},
+			pattern: {
+				message: 'pattern'
+			},
+			min: {
+				message: 'min'
+			},
+			max: {
+				message: 'max'
+			},
+			minlength: {
+				message: 'Too short.'
+			},
+			maxlength: {
+				message: 'Too long.'
+			}
+		},
 		custom : function(attributeName, model, valueToSet,options) {
 			return model[options.methodName](attributeName, valueToSet);
 		},
@@ -104,7 +228,7 @@ Backbone.Model = Backbone.Model.extend({
 			var isNotAlreadySet = _.isUndefined(currentValue);
 			var isNotBeingSet = _.isUndefined(valueToSet);
 			if (_.isNull(valueToSet) || valueToSet === "" || (isNotBeingSet && isNotAlreadySet)) {
-				return "required";
+				return this.errors.required;
 			}
 			return false;
 		},
@@ -112,13 +236,13 @@ Backbone.Model = Backbone.Model.extend({
 			var emailRegex = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
 
 			if (_.isString(valueToSet) && !valueToSet.match(emailRegex)) {
-				return "email";
+				return this.errors.email;
 			} else return false;
 		},
 		url : function(attributeName, model, valueToSet) {
 			var urlRegex = /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i;
 			if (_.isString(valueToSet) && !valueToSet.match(urlRegex)) {
-				return "url";
+				return this.errors.url;
 			} else return false;
 		},
 		pattern : function(attributeName, model, valueToSet,options) {
@@ -126,29 +250,29 @@ Backbone.Model = Backbone.Model.extend({
 				if (valueToSet.match(options.pattern)) {
 					return false;
 				} else {
-					return "pattern";
+					return this.errors.pattern;
 				}
 			} else return false;
 		},
 		min : function(attributeName, model, valueToSet,options) {
 			if (valueToSet < options.minimumValue) {
-				return "min";
+				return this.errors.min;
 			} else return false;
 		},
 		max : function(attributeName, model, valueToSet,options) {
 			if (valueToSet > options.maximumValue) {
-				return "max";
+				return this.errors.max;
 			} else return false;
 		},
 		minlength : function( attributeName, model, valueToSet,options) {
 			if (_.isString(valueToSet)) {
-				if (valueToSet.length < options.minlength) return "minlength";
+				if (valueToSet.length < options.minlength) return this.errors.minlength;
 			}
 			return false;
 		},
 		maxlength : function( attributeName, model, valueToSet,options) {
 			if (_.isString(valueToSet)) {
-				if (valueToSet.length > options.maxlength) return "maxlength";
+				if (valueToSet.length > options.maxlength) return this.errors.maxlength;
 			}
 			return false;
 		}
@@ -156,95 +280,33 @@ Backbone.Model = Backbone.Model.extend({
 });
 
 
-/**
- * Form view
- */
-Backbone.Form = Backbone.View.extend({
-	dataKey: 'backbraceinput',
-	events: {
-		"focus .field": "focusField",
-		"blur .field": "blurField",
-		"change .field":"changeField",
-		"submit":"submitForm"
-	},
-	fields: {},
-	initialize: function () {
-		this.model = new (this.model)()
-		this.el = $(this.el);
 
-		// Populate fields
-		var view = this;
-		_.each(this.model.rules,function (value,field) {
-			var element = view.el.find(".field."+field);
-			element.data(view.dataKey,field);
-			view.fields[field]=element;
-		})
-		_.bindAll(this);
+// Bonus?
+// This should probably be put somewhere else,
+// but it's included because it's userful
+// Adds a max and minimum to backbone collections
+Backbone.Collection = Backbone.Collection.extend({
+	max: 100,
+	min: 0,
+	// Add without overflowing
+	safeAdd: function (o) {
+		if (this.length < this.max) {
+			this.add(o);
+			if (this.length==this.max && this.onFull) this.onFull();
+			return true;
+		} else return false;
 	},
-	focusField: function (e) {
-		this.undecorateField($(e.currentTarget));
-	},
-	blurField: function (e) {
-		this.validateField($(e.currentTarget));
-	},
-	changeField: function (e) {
-		this.validateField($(e.currentTarget));
-	},
-	submitForm: function(e) {
-		if (!this.validateFields()) {
-			// Allow traditionals form submission by default
-			// you can override this with an AJAX submission if you'd like
+	// Remove without overflowing
+	safeRemove: function (o) {
+		if (o && o.length && this.length-o.length >= this.min) {
+			this.remove(o);
 			return true;
 		}
-		else {
-			// Prevent form submission
-			e.preventDefault();
-			return false;
+		else if (o && this.length-1 >= this.min) {
+			this.remove(o)
+			return true;
 		}
-	},
-	validateField: function(element) {
-		var field = element.data(this.dataKey),
-		input = this.fields[field],
-		hasError;
-		if (field) {
-			hasError = this.model.validateOne(field,input.val());
-			this.renderField(hasError,input,field);
-		}
-		return !field || hasError;
-	},
-	renderField: function(error,elem,field) {
-		if (error)
-			this.decorateField(elem,error);
 		else
-			this.undecorateField(elem,error);
-	},
-	validateFields: function () {
-		// Get values from form
-		var view = this;
-		var data= {},
-		hasError = false;
-		_.each(this.model.rules,function (rule,field) {
-			var elem = view.el.find(".field."+field);
-			hasError = view.validateField(elem) || hasError;
-			data[field] = view.el.find(".field."+field).val();
-		});
-		this.model.set(data);
-		return hasError;
-	},
-
-	// Presentation logic for error reporting
-
-	/**
-	 * element -> the form element that is naughty
-	 * error   -> the string key of the validation fail that was triggered
-	 *				( i.e. required, email, url, minLength, etc. )
-	 */
-	decorateField: function(element,error) {
-		element.addClass('error')
-		element.parent().addClass('error');
-	},
-	undecorateField: function(element,error) {
-		element.removeClass('error');
-		element.parent().removeClass('error');
+			return false;
 	}
-});
+})
--- example.js
+++ example.js
@@ -1,3 +1,4 @@
+// Build your domain model with defaults and validation rules
 var Example = Backbone.Model.extend({
 	initialize: function(obj) {
 		this.body = obj;
@@ -11,30 +12,66 @@ var Example = Backbone.Model.extend({
 		newsletter:''
 	},
 	rules: {
-		email: ["required","email"],
-		url: ["required","url"],
+		email: ["email","required"],
+		url: ["url","required"],
 		username: [
-			"required",
-			{
-				name:"minlength",
-				minlength: 3
-			},
-			{
-				name:"maxlength",
-				maxlength: 15
-			}
+		{
+			name:"minlength",
+			minlength: 3
+		},
+		{
+			name:"maxlength",
+			maxlength: 15
+		},
+		"required"
 		],
-		password: "required"
+		password: ["required"]
 	}
 });
+// Create, but don't use the form as your view
 var ExampleForm = Backbone.Form.extend({
 	model: Example,
 	el: "form.example"
 });
+// Instead create a formview to encapsulate the form logic
+var ExampleFormView = Backbone.View.extend({
+	initialize: function () {
+		// Make the form
+		this.form = new ExampleForm();
+
+		// Assign submission event
+		this.form.submit = function () {
+			var result=$("#form-result");
+			var finish = function () {
+				result.addClass('success').removeClass('error').fadeIn(800).text('Success!');
+			}
+
+			if (!result.hasClass('success'))
+				result.fadeOut(100,finish);
+			else finish();
+		}
+		// Assign error event
+		this.form.error = function () {
+			var result=$("#form-result");
+			var finish = function (){
+				result.addClass('error').fadeIn(800).text('Error!');
+			}
+			
+			if (!result.hasClass('error'))
+				result.fadeOut(100,finish);
+			else finish();
+		}
+
+		// *****
+		// Notice, I didn't override the decorateField and undecorateField events.
+		// That's because I like them just how they are, thanks.
+
+	}
+})
 
 
 // Initialize
 $(function () {
-	new ExampleForm();
+	new ExampleFormView();
 })
 
